329519855c2161c9ca73272009ed1a744e3a4adc
wycliffetyrell <wycliffebundi3@gmail.com>
2024-02-22 14:43:27 +0300

    python assignment week 1

---
+++ python_assignment_week1.py
@@ -0,0 +1,11 @@
+#Ask name
+name = input('Enter your name :')
+
+# Ask age
+age = input('Enter your age :')
+
+# Ask Location 
+location = input('Where do you live:')
+#print the imputs
+
+print(' Hello {}. I hope you are having a wonderful day ? we are reminding you you have turned {} and your birthday will be at {}'.format(name,age,location))
